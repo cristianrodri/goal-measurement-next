4e886389b4fb4a00be904113d5c9fbe76adacb51
Cristian Rodriguez <contacto.cristianrodriguez@gmail.com>
2022-09-23 15:06:04 -0300

    Setting up testing workflow

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest'
+  },
+  setupFilesAfterEnv: ['./jest.setup.ts']
+}
